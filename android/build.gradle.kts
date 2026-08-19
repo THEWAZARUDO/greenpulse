801d604e18a7191ae94cee60f5209a396998a424
THEWAZARUDO <j404sans@gmail.com>
2026-08-19 13:09:12 +0700

    fix(android): Xóa khối dependencies không hợp lệ trong root build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,15 +21,4 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-dependencies {
-    // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:34.17.0"))
-
-    // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation("com.google.firebase:firebase-auth")
-
-    // Also add the dependency for the Google Play services library and specify its version
-    implementation("com.google.android.gms:play-services-auth:21.6.0")
-}
+}
